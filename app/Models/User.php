4791902a5aa2b83cd76327a6339fc29b038baa5e
Mwangi <gideonmwangi718@gmail.com>
2024-06-11 17:19:19 +0300

    Eloquent laravel relationships examples

---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = ['username', 'email', 'password'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
